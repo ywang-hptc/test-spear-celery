4df244f0ec76ba741708e84584e3a86420e75613
Yibing Wang <y.wang@hollandptc.nl>
2025-03-07 09:34:49 +0100

    finish unittest for api

--- app/spear_job_api/tests/test_api.py
+++ app/spear_job_api/tests/test_api.py
@@ -4,13 +4,16 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from spear_job_api.models import SpearJob
 from spear_job_api.serializers import SpearJobSerializer
+from django.utils import timezone
+import pytz
+import datetime
 
 SPEAR_JOB_URL = reverse("spear_job_api:spearjob-list")
 
 
 def detail_url(celery_id: str):
     """Create and return a recipe detail URL."""
-    return reverse("spear_job:spearjob-detail", args=[celery_id])
+    return reverse("spear_job_api:spearjob-detail", args=[celery_id])
 
 
 def create_spear_job(**params):
@@ -44,69 +47,79 @@ class SpearJobApiTests(TestCase):
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         spear_job = SpearJob.objects.get(id=res.data["id"])
-        print(res.data)
         for k, v in payload.items():
             self.assertEqual(getattr(spear_job, k), v)
 
-    def test_get_recipe_detail(self):
-        """Test get recipe detail"""
-        recipe = create_recipe(user=self.user)
-        url = detail_url(recipe.id)
+    def test_get_spear_job_detail(self):
+        """Test get the detail of the spear job"""
+        spear_job = create_spear_job(celery_job_id="test1234")
+        url = detail_url(celery_id="test1234")
         res = self.client.get(url)
-        serializer = RecipeDetailSerializer(recipe)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        serializer = SpearJobSerializer(spear_job)
         self.assertEqual(res.data, serializer.data)
 
-    # TODO: implement below
-    # def test_partial_update_recipe(self):
-    #     """Test updating a recipe with patch"""
-    #     original_link = "https://example.com/recipe.pdf"
-    #     recipe = create_recipe(
-    #         user=self.user, title="Sample recipe title", link=original_link
-    #     )
-    #     payload = {"title": "New recipe title"}
-    #     url = detail_url(recipe.id)
+    def test_partial_update_spear_job_prerun(self):
+        """Test updating a spear job data with patch, changing status to RUNNING"""
+        spear_job = create_spear_job(celery_job_id="test4321")
+        url = detail_url(celery_id="test4321")
+        payload = {
+            "status": "RUNNING",
+            "started_at": datetime.datetime(
+                2023, 11, 10, 0, 0, 0, tzinfo=pytz.utc
+            ).isoformat(),
+            "worker_name": "workerA",
+        }
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        spear_job.refresh_from_db()  # refresh the recipe object from the DB
+        self.assertEqual(spear_job.status, "RUNNING")
+        self.assertEqual(
+            spear_job.started_at,
+            datetime.datetime(2023, 11, 10, 0, 0, 0),
+        )
+        self.assertEqual(spear_job.worker_name, "workerA")
 
-    #     res = self.client.patch(url, payload)
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
-    #     recipe.refresh_from_db()  # refresh the recipe object from the DB
-    #     self.assertEqual(recipe.title, payload["title"])
-    #     self.assertEqual(recipe.link, original_link)
-    #     self.assertEqual(recipe.user, self.user)
+    def test_partial_update_spear_job_postrun_success(self):
+        """Test updating a spear job data with patch, changing status to SUCCESS"""
+        spear_job = create_spear_job(celery_job_id="test43210")
+        url = detail_url(celery_id="test43210")
+        payload = {
+            "status": "SUCCESS",
+            "completed_at": datetime.datetime(
+                2023, 11, 10, 0, 0, 0, tzinfo=pytz.utc
+            ).isoformat(),
+            "result": "test_job_result",
+        }
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        spear_job.refresh_from_db()  # refresh the recipe object from the DB
+        self.assertEqual(spear_job.status, "SUCCESS")
+        self.assertEqual(
+            spear_job.completed_at,
+            datetime.datetime(2023, 11, 10, 0, 0, 0),
+        )
+        self.assertEqual(spear_job.result, "test_job_result")
+        self.assertIsNone(spear_job.error)
 
-    # def test_full_update(self):
-    #     """Test full update of recipe."""
-    #     recipe = create_recipe(
-    #         user=self.user,
-    #         title="Sample recipe title",
-    #         time_minutes=22,
-    #         price=Decimal("5.25"),
-    #         description="Sample recipe description",
-    #         link="https://example.com/recipe.pdf",
-    #     )
-
-    #     payload = {
-    #         "title": "New recipe title",
-    #         "time_minutes": 25,
-    #         "price": Decimal("6.25"),
-    #         "description": "New description",
-    #         "link": "https://example.com/updated-recipe.pdf",
-    #     }
-
-    #     url = detail_url(recipe.id)
-    #     res = self.client.put(url, payload)
-    #     self.assertEqual(res.status_code, status.HTTP_200_OK)
-    #     recipe.refresh_from_db()
-
-    #     for k, v in payload.items():
-    #         self.assertEqual(getattr(recipe, k), v)
-    #     self.assertEqual(recipe.user, self.user)
-
-    # def test_delete_recipe(self):
-    #     """Test deleting a recipe"""
-    #     recipe = create_recipe(user=self.user)
-    #     url = detail_url(recipe.id)
-    #     res = self.client.delete(url)
-
-    #     # successful delete should return 204
-    #     self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-    #     self.assertFalse(Recipe.objects.filter(id=recipe.id).exists())
+    def test_partial_update_spear_job_postrun(self):
+        """Test updating a spear job data with patch, changing status to FAILURE"""
+        spear_job = create_spear_job(celery_job_id="test543210")
+        url = detail_url(celery_id="test543210")
+        payload = {
+            "status": "FAILURE",
+            "completed_at": datetime.datetime(
+                2023, 11, 10, 0, 0, 0, tzinfo=pytz.utc
+            ).isoformat(),
+            "error": "test_job_error_message",
+        }
+        res = self.client.patch(url, payload)
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        spear_job.refresh_from_db()  # refresh the recipe object from the DB
+        self.assertEqual(spear_job.status, "FAILURE")
+        self.assertEqual(
+            spear_job.completed_at,
+            datetime.datetime(2023, 11, 10, 0, 0, 0),
+        )
+        self.assertIsNone(spear_job.result)
+        self.assertEqual(spear_job.error, "test_job_error_message")
